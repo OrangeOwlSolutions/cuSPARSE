da2f01b6b832526c8ce70d20f3f88c7c48f00933
Orange Owl <orangeowlsolutions@gmail.com>
2019-01-15 11:58:27 +0100

    Update sparseMatrixAddition.cu

--- Operations/sparseMatrixAddition.cu
+++ Operations/sparseMatrixAddition.cu
@@ -106,19 +106,19 @@ int main() {
 	int *h_csrRowPtrB = (int *)malloc((M + 1) * sizeof(int));
 	int *h_csrColIndB = (int *)malloc(nnz1 * sizeof(int));
 
-	h_csrValA[0] = 1.f; h_csrValA[1] = 7.f; h_csrValA[2] = 1.f; h_csrValA[3] = 3.f; h_csrValA[4] = -1.f; 
+	h_csrValA[0] = 1.f; h_csrValA[1] = 7.f; h_csrValA[2] = 1.f; h_csrValA[3] = 3.f; h_csrValA[4] = -1.f;
 	h_csrValA[5] = 10.f; h_csrValA[6] = 1.f; h_csrValA[7] = -4.f; h_csrValA[8] = 1.f; h_csrValA[9] = 3.f;
 
-	h_csrRowPtrA[0] = 0; h_csrRowPtrA[1] = 3; h_csrRowPtrA[2] = 5; h_csrRowPtrA[3] = 6; 
+	h_csrRowPtrA[0] = 0; h_csrRowPtrA[1] = 3; h_csrRowPtrA[2] = 5; h_csrRowPtrA[3] = 6;
 	h_csrRowPtrA[4] = 8; h_csrRowPtrA[5] = 10;
 
 	h_csrColIndA[0] = 0; h_csrColIndA[1] = 3; h_csrColIndA[2] = 5; h_csrColIndA[3] = 2; h_csrColIndA[4] = 4;
 	h_csrColIndA[5] = 1; h_csrColIndA[6] = 0; h_csrColIndA[7] = 3; h_csrColIndA[8] = 3; h_csrColIndA[9] = 5;
 
-	h_csrValB[0] = 3.f;	h_csrValB[1] = 1.f;	h_csrValB[2] = -1.f; h_csrValB[3] = 1.f; 
+	h_csrValB[0] = 3.f;	h_csrValB[1] = 1.f;	h_csrValB[2] = -1.f; h_csrValB[3] = 1.f;
 	h_csrValB[4] = -4.f; h_csrValB[5] = -3.f; h_csrValB[6] = -2.f; h_csrValB[7] = 10.f;
 
-	h_csrRowPtrB[0] = 0; h_csrRowPtrB[1] = 2; h_csrRowPtrB[2] = 4; h_csrRowPtrB[3] = 5;	
+	h_csrRowPtrB[0] = 0; h_csrRowPtrB[1] = 2; h_csrRowPtrB[2] = 4; h_csrRowPtrB[3] = 5;
 	h_csrRowPtrB[4] = 7; h_csrRowPtrB[5] = 8;
 
 	h_csrColIndB[0] = 0; h_csrColIndB[1] = 4; h_csrColIndB[2] = 0; h_csrColIndB[3] = 1; h_csrColIndB[4] = 3;
@@ -141,13 +141,21 @@ int main() {
 	gpuErrchk(cudaMemcpy(d_csrRowPtrB, h_csrRowPtrB, (M + 1) * sizeof(int), cudaMemcpyHostToDevice));
 	gpuErrchk(cudaMemcpy(d_csrColIndB, h_csrColIndB, nnz2 * sizeof(int), cudaMemcpyHostToDevice));
 
+	// --- Transforming matrix A from csr to dense format
+	float *d_A;				gpuErrchk(cudaMalloc(&d_A, M * N * sizeof(float)));
+	cusparseSafeCall(cusparseScsr2dense(handle, M, N, descrA, d_csrValA, d_csrRowPtrA, d_csrColIndA, d_A, M));
+
+	// --- Transforming matrix B from csr to dense format
+	float *d_B;				gpuErrchk(cudaMalloc(&d_B, M * N * sizeof(float)));
+	cusparseSafeCall(cusparseScsr2dense(handle, M, N, descrB, d_csrValB, d_csrRowPtrB, d_csrColIndB, d_B, M));
+
 	// --- Summing the two matrices
 	int baseC, nnz3;
 	// --- nnzTotalDevHostPtr points to host memory
 	int *nnzTotalDevHostPtr = &nnz3;
 	cusparseSafeCall(cusparseSetPointerMode(handle, CUSPARSE_POINTER_MODE_HOST));
 	int *d_csrRowPtrC;	 gpuErrchk(cudaMalloc(&d_csrRowPtrC, (M + 1) * sizeof(int)));
-	cusparseSafeCall(cusparseXcsrgeamNnz(handle, M, N, descrA, nnz1, d_csrRowPtrA, d_csrColIndA, descrB, 
+	cusparseSafeCall(cusparseXcsrgeamNnz(handle, M, N, descrA, nnz1, d_csrRowPtrA, d_csrColIndA, descrB,
 		nnz2, d_csrRowPtrB, d_csrColIndB, descrC, d_csrRowPtrC, nnzTotalDevHostPtr));
 	if (NULL != nnzTotalDevHostPtr) { nnz3 = *nnzTotalDevHostPtr; }
 	else{
@@ -158,18 +166,36 @@ int main() {
 	int *d_csrColIndC;	 gpuErrchk(cudaMalloc(&d_csrColIndC, nnz3 * sizeof(int)));
 	float *d_csrValC;	 gpuErrchk(cudaMalloc(&d_csrValC, nnz3 * sizeof(float)));
 	float alpha = 1.f, beta = 1.f;
-	cusparseSafeCall(cusparseScsrgeam(handle, M, N, &alpha, descrA, nnz1, d_csrValA, d_csrRowPtrA, 
-		d_csrColIndA, &beta,	descrB, nnz2, d_csrValB, d_csrRowPtrB, d_csrColIndB, descrC, d_csrValC, 
+	cusparseSafeCall(cusparseScsrgeam(handle, M, N, &alpha, descrA, nnz1, d_csrValA, d_csrRowPtrA,
+		d_csrColIndA, &beta, descrB, nnz2, d_csrValB, d_csrRowPtrB, d_csrColIndB, descrC, d_csrValC,
 		d_csrRowPtrC, d_csrColIndC));
 
 	// --- Transforming csr to dense format
 	float *d_C;				gpuErrchk(cudaMalloc(&d_C, M * N * sizeof(float)));
 	cusparseSafeCall(cusparseScsr2dense(handle, M, N, descrC, d_csrValC, d_csrRowPtrC, d_csrColIndC, d_C, M));
 
+	float *h_A = (float *)malloc(M * N * sizeof(float));
+	gpuErrchk(cudaMemcpy(h_A, d_A, M * N * sizeof(float), cudaMemcpyDeviceToHost));
+	float *h_B = (float *)malloc(M * N * sizeof(float));
+	gpuErrchk(cudaMemcpy(h_B, d_B, M * N * sizeof(float), cudaMemcpyDeviceToHost));
 	float *h_C = (float *)malloc(M * N * sizeof(float));
 	gpuErrchk(cudaMemcpy(h_C, d_C, M * N * sizeof(float), cudaMemcpyDeviceToHost));
 
 	// --- m is row index, n column index
+	for (int m = 0; m < M; m++) {
+		for (int n = 0; n < N; n++) { printf("%f ", h_A[m + n * M]); }
+		printf("\n");
+	}
+
+	// --- m is row index, n column index
+	printf("\n");
+	for (int m = 0; m < M; m++) {
+		for (int n = 0; n < N; n++) { printf("%f ", h_B[m + n * M]); }
+		printf("\n");
+	}
+
+	// --- m is row index, n column index
+	printf("\n");
 	for (int m = 0; m < M; m++) {
 		for (int n = 0; n < N; n++) { printf("%f ", h_C[m + n * M]); }
 		printf("\n");
@@ -177,3 +203,4 @@ int main() {
 
 	return 0;
 }
+
